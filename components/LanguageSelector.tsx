482c1c4eba50bc53db60b071a01af1c5605e64df
Ramanand Thakur <ramanandt35@gmail.com>
2023-12-20 18:53:08 +0530

    language dropdown optimized

--- components/LanguageSelector.tsx
+++ components/LanguageSelector.tsx
@@ -19,6 +19,7 @@ const LanguageSelector = ({ language, setLanguage, setActiveIcon }: LanguageSele
         if (newActiveIcon) {
             setActiveIcon(newActiveIcon)
         }
+        toggleDropdown();
     }
 
     const toggleDropdown = () => {
@@ -26,20 +27,23 @@ const LanguageSelector = ({ language, setLanguage, setActiveIcon }: LanguageSele
     }
 
     return (
-        <div onClick={toggleDropdown}>
-            <p className="text-sm font-medium py-[5px] text-slate-300">Languages</p>
-            <div className=" p-1 dropdown-title capitalize w-[105px]">
+        <div>
+            <p className="text-sm font-medium py-[5px] text-slate-300 overflow-hidden">Languages</p>
+            <div onClick={toggleDropdown} className=" p-1 dropdown-title capitalize w-[105px]">
                 {language}
                 <ChevronDown />
             </div>
             {showDropdown &&
-                <div>
+                <div className='dropdown-menu'>
                     {languages.map((lang, i) => {
                         return (
                             <div key={i} className=" rounded-md transition duration-75 hover:border-slate-400 
-                            hover:border-b-2 mt-2 hover:border-r-2 dropdown-item text-left w-[105px]
-                            hover:bg-slate-700 flex justify-between p-1">
-                                <button onClick={() => handleLanguageChange(lang.name)}>
+                            hover:border-b-2 mt-2 hover:border-r-2 dropdown-item text-left w-[105px] items-center
+                            hover:bg-slate-700 flex overflow-hidden">
+                                <button
+                                className="text-sm  text-slate-300 flex items-center pr-[105px]
+                                "
+                                onClick={() => handleLanguageChange(lang.name)}>
                                     {lang.name}
                                 </button>
                             </div>
