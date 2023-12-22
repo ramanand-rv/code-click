dff5cfbf2f9a75bef67e3ee309628b93a827993b
Ramanand Thakur <ramanandt35@gmail.com>
2023-12-22 18:53:30 +0530

    padding updated

--- components/PaddingSelector.tsx
+++ components/PaddingSelector.tsx
@@ -10,45 +10,30 @@ interface PaddingSelectorProps {
     setCurrentPadding: (padding: string) => void;
 }
 
-const PaddingSelector = ({paddings, currentPadding, setCurrentPadding}: PaddingSelectorProps) => {
-    const [showDropdown, setShowDropdown] = React.useState(false);
-
-    const toggleDropdown = () => {
-        setShowDropdown(!showDropdown);
-    }
-
+const PaddingSelector = ({ paddings, currentPadding, setCurrentPadding }: PaddingSelectorProps) => {
     const handlePaddingChange = (newPadding: string) => {
         setCurrentPadding(newPadding);
-        toggleDropdown();
     }
 
-  return (
-      <OutsideClickHandler onOutsideClick={() => setShowDropdown(false)}>
-          <div className='theme-selector'>
-              <p className="text-sm font-medium py-[5px] text-slate-300 overflow-hidden">Padding</p>
+    return (
+            <div className='theme-selector'>
+                <p className="text-sm font-medium py-[5px] text-slate-300 overflow-hidden">Padding</p>
 
-              <div onClick={toggleDropdown} className=" p-1 dropdown-title capitalize w-[160px]">
-                  {currentPadding}
-                  <ChevronDown />
-              </div>
+                <div className="flex gap-6 ">
+                    {paddings.map((pad, i)=>{
+                        return (
+                            <button key={i}
+                            onClick={()=>handlePaddingChange(pad)}
+                            className={`h-9 flex items-center border-[1px] rounded border-slate-700 justify-center text-sm px-2
+                            ${currentPadding === pad && 'bg-slate-500  text-white'}`}>
+                                    {pad}
+                            </button>
+                        )
+                    })}
+                </div>
 
-              {showDropdown &&
-                  <div className='dropdown-menu' >
-                      {paddings.map((pad, i) => {
-                          return (
-                              <button className='rounded-md transition duration-75 hover:border-slate-400 text-sm text-slate-300 items-center 
-                            hover:border-b-2 mt-2 hover:border-r-2 capitalize dropdown-item text-left w-[160px]
-                            hover:bg-slate-700 flex overflow-hidden' onClick={() => handlePaddingChange(pad)}>
-                                  {pad}
-                              </button>
-                          )
-
-                      })}
-                  </div>
-              }
-          </div>
-      </OutsideClickHandler>
-  )
+            </div>
+    )
 }
 
 export default PaddingSelector
